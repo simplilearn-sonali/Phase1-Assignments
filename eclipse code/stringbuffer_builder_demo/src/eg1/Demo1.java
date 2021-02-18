cbf53c30ae4454df57d6ab2da6282af2f843d639
sonalikastoori <sonalikastoori97@gmail.com>
2021-02-18 16:18:43 -0500

    string demo

--- eclipse code/stringbuffer_builder_demo/src/eg1/Demo1.java
+++ eclipse code/stringbuffer_builder_demo/src/eg1/Demo1.java
@@ -3,8 +3,29 @@ package eg1;
 public class Demo1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		StringBuffer sb=new StringBuffer("hello");
+		System.out.println(sb);
+		sb.append(" hey");
+		System.out.println(sb);
+		sb.append(true).append("upper").append(123111).append(9090);
+		System.out.println(sb);
+		sb.reverse();
+		System.out.println(sb);
+		sb.insert(1, "JAVA");
+		System.out.println(sb);
+		sb.deleteCharAt(0);
+		System.out.println(sb);
+		sb.delete(1, 4);
+		System.out.println(sb);
+		
+		
+		StringBuffer sb1=new StringBuffer("Java");
+		StringBuffer sb2=new StringBuffer("JavaSE");
+		if(sb1.toString().equals(sb2.toString())) {
+			System.out.println("both are equal");
+		}else {
+			System.out.println("both are not equal");
+		}
 	}
 
 }
